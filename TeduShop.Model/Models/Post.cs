8470da23c0a11e1c4334dd06cd37363117d3898f
jacky181 <huatrunghieu181@gmail.com>
2019-05-02 17:21:09 +0700

    sửa tên abstracts

--- TeduShop.Model/Models/Post.cs
+++ TeduShop.Model/Models/Post.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using TeduShop.Model.Abstract;
+using TeduShop.Model.Abstracts;
 
 namespace TeduShop.Model.Models
 {
--- TeduShop.Model/Models/ProductCategory.cs
+++ TeduShop.Model/Models/ProductCategory.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using TeduShop.Model.Abstract;
 using TeduShop.Model.Abstracts;
+
 
 namespace TeduShop.Model.Models
 {
