c699bc2afa3373bb1a56fa669c20f4346b78fe3b
jacky181 <huatrunghieu181@gmail.com>
2019-05-02 17:06:25 +0700

    define model

--- TeduShop.Model/Models/MenuGroup.cs
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,13 +7,13 @@ namespace TeduShop.Model.Models
 {
     [Table("MenuGroups")]
     public class MenuGroup
-
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { set; get; }
 
         [Required]
+        [MaxLength(50)]
         public string Name { set; get; }
 
         public virtual IEnumerable<Menu> Menus { set; get; }
--- TeduShop.Model/Models/PostCategory.cs
+++ TeduShop.Model/Models/PostCategory.cs
@@ -2,12 +2,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using TeduShop.Model.Abstract;
-using TeduShop.Model.Abstracts;
 
 namespace TeduShop.Model.Models
 {
-    [Table("ProductCategories")]
-    public class ProductCategory : Auditable
+    [Table("PostCategories")]
+    public class PostCategory : Auditable
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -18,11 +17,13 @@ namespace TeduShop.Model.Models
         public string Name { set; get; }
 
         [Required]
+        [Column(TypeName = "varchar")]
         [MaxLength(256)]
         public string Alias { set; get; }
 
         [MaxLength(500)]
         public string Description { set; get; }
+
         public int? ParentID { set; get; }
         public int? DisplayOrder { set; get; }
 
@@ -31,6 +32,6 @@ namespace TeduShop.Model.Models
 
         public bool? HomeFlag { set; get; }
 
-        public virtual IEnumerable<Product> Products { set; get; }
+        public virtual IEnumerable<Post> Posts { set; get; }
     }
 }
